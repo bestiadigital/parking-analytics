b9c11ce679475c48aeecd2bd8fe333232ad0bf63
bestiadigital <lahurt@gmail.com>
2026-04-06 13:04:24 -0300

    cambio 3
    3

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Esto asegura que las rutas a los archivos JS/CSS sean relativas
 })
